93cbf3f0e1fcadc2591855e364050cac38fc8abd
Altaf Ahmed <90945964+AltafAhmedGeek@users.noreply.github.com>
2024-11-04 16:21:39 +0530

    Update UserMasterController.php

--- app/Http/Controllers/UserMasterController.php
+++ app/Http/Controllers/UserMasterController.php
@@ -29,7 +29,7 @@ class UserMasterController extends Controller
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
-        dd($validator,$validator->fails());
+        dd($validator);
 
         if ($validator->fails()) {
             return redirect()->back()
