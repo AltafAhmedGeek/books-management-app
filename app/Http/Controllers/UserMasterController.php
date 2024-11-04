4c6c3244119bd3191866cd4ab9df47a5e8c569f1
Altaf Ahmed <90945964+AltafAhmedGeek@users.noreply.github.com>
2024-11-04 16:11:53 +0530

    Update UserMasterController.php

--- app/Http/Controllers/UserMasterController.php
+++ app/Http/Controllers/UserMasterController.php
@@ -23,7 +23,7 @@ class UserMasterController extends Controller
      */
     public function store(Request $request)
     {
-        //dd($request);
+        dd($request);
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
